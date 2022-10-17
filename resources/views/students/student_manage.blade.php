012fa2423aa61f433ec5bbdd71d7f7b514671a57
hieu121202 <hieu62605@gmail.com>
2022-10-17 07:48:40 +0700

    first commit

---
+++ resources/views/students/student_manage.blade.php
@@ -0,0 +1,38 @@
+@extends('layout.app')
+
+@section('content')
+
+    <div class="text-center m-5">
+        <a href="{{ URL::to('/students/add') }}" class="btn btn-outline-primary">Add student</a>
+    </div>
+    <div class="container">
+        <table class="table">
+            <?php $index = 1; ?>
+            <thead>
+                <tr class="table-dark">
+                    <th scope="col">#</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Fullname</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Birthday</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $std)
+                    <tr>
+                        <th scope="row">{{ $index++ }}</th>
+                        <td>{!! $std->id !!}</td>
+                        <td>{!! $std->fullname !!}</td>
+                        <td>{!! $std->address !!}</td>
+                        <td><?php echo explode(' ', $std->birthday)[0]; ?></td>
+                        <td>
+                            <a href="students/{!! $std->id !!}/edit" class="btn btn-outline-primary">Edit</a>
+                            <a href="students/{!! $std->id !!}/delete" class="btn btn-outline-danger">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
